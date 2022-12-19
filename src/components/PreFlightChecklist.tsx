0cb87f1e063fefe3567991192c7f142677563bc4
EcaCosca <enriquecoscarelli@gmail.com>
2022-12-19 23:01:59 +0100

    Adding more functionality to the preflight

--- src/components/PreFlightChecklist.tsx
+++ src/components/PreFlightChecklist.tsx
@@ -6,6 +6,8 @@ const PreFlightChecklist = () => {
     const {preFlightChecklist} = useSelector((state) => state.preFlightChecklist);
     const dispatch = useDispatch();
 
+    console.log(preFlightChecklist)
+
   return (
     <div>
         <h2>PreFlightChecklist</h2>
@@ -13,9 +15,9 @@ const PreFlightChecklist = () => {
         <ol>
           {preFlightChecklist.map(item => 
             <li id={item.id}>
-                {item.title}
-                {item.id}
+                <input onChange={(e) => dispatch(update({id:item.id, title:e.target.value}))}type="text" id="disabled-input" aria-label="disabled input" className="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value={item.title}></input>
                 <button onClick={() => dispatch(erase(item.id))}>Erase</button>
+                {/* <button onClick={() => dispatch(erase(item.id))}>Edit</button> */}
             </li>
           )}
         </ol>
